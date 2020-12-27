b5d44c31069b713e0aabba6ba8a7bbb13e67758b
tuhas <rileydeq@gmail.com>
2020-12-28 00:45:49 +0300

    add info logging

--- check_people.py
+++ check_people.py
@@ -1,6 +1,6 @@
 import argparse
-import logging
 import csv
+import logging
 from pathlib import Path
 
 import vk
@@ -20,11 +20,13 @@ def get_user_id(vk_api: vk.api.API, vk_id: str) -> int:
     formatted_id = vk_id.replace(" ", "").replace("@", "")
     id_split = formatted_id.split("/")
     target_id = id_split[-1] if id_split[-1] != "" else id_split[-2]
+    logger.info(f"file-id: '{vk_id}'. Transformed id: '{target_id}'")
     user_info = vk_api.users.get(user_ids=[target_id], v=API_VERSION)
     if len(user_info) > 1:
         msg = f"Invalid length of user info list. id={vk_id}"
         raise ValueError(msg)
     valid_user_id = user_info[0]["id"]
+    logger.info(f"file-id: '{vk_id}'. Valid VK id: '{valid_user_id}'")
     return valid_user_id
 
 
@@ -45,25 +47,30 @@ def main(file_path: str, result_path: str) -> None:
                          desc="Users processing"):
         col_id = row[COLUMN_NAME_VK_ID]
         book_number = row[COLUMN_NAME_BOOKNUMBER]
+        logger.info(f"start processing file-id: '{col_id}'")
         if len(data_frame[(data_frame[COLUMN_NAME_BOOKNUMBER] == book_number) &
                           (data_frame[COLUMN_NAME_STATUS] != '')].index) > 0:
             data_frame.at[idx, COLUMN_NAME_STATUS] = STATUS_DUPLICATE
+            logger.info(f"file-id: '{col_id}'. Found duplicate")
             continue
         try:
             valid_id = get_user_id(vk_api, col_id)
         except Exception as err:
             data_frame.at[idx, COLUMN_NAME_ERROR_MSG] = err
             data_frame.at[idx, COLUMN_NAME_STATUS] = STATUS_ERROR
-            logger.exception("Error")
+            logger.exception(f"file-id: '{col_id}'. Error")
             continue
         if valid_id in data:
             data_frame.at[idx, COLUMN_NAME_STATUS] = STATUS_GROUP_MEMBER
+            logger.info(f"file-id: '{col_id}'. Set status '{STATUS_GROUP_MEMBER}'")
         else:
             data_frame.at[idx, COLUMN_NAME_STATUS] = STATUS_NOT_MEMBER
+            logger.info(f"file-id: '{col_id}'. Set status '{STATUS_NOT_MEMBER}'")
     data_frame.to_csv(result_path, sep=",", header=True, index=False,
                       encoding='utf-8', quoting=csv.QUOTE_MINIMAL,
                       quotechar='"')
     res_path = Path(result_path).resolve()
+    logger.info(f"End processing. Save file to {res_path}")
     print(f'Result file was saved to {res_path}')
 
 
